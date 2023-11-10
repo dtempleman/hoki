39ae82e7b600cf10d650b76ddc2946824cef46bd
Daniel <dtempleman@kinsolresearch.com>
2023-11-09 18:18:52 -0800

    - added createinitialsuperuser command

---
+++ api/pawn/management/commands/createinitialsuperuser.py
@@ -0,0 +1,20 @@
+import os
+
+from django.contrib.auth.management.commands import createsuperuser
+
+
+class Command(createsuperuser.Command):
+    help = "Create a superuser, and exit gracefully if username already exists."
+
+    def handle(self, *args, **options):
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        database = "default"  # os.getenv("POSTGRES_DB")
+        exists = (
+            self.UserModel._default_manager.db_manager(database)
+            .filter(username=username)
+            .exists()
+        )
+        if exists:
+            self.stdout.write(f"User {username} already exists, exiting normally.")
+
+        super(Command, self).handle(*args, **options)
--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -4,4 +4,14 @@ cmd="$@"
 
 /wait-for-it.sh -t 10 $DATABASE_HOST:$DATABASE_PORT
 
-python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createinitialsuperuser --noinput
+
+
+if [ -z "$cmd" ]
+then
+	python manage.py runserver 0.0.0.0:8000
+else
+	exec $cmd
+fi
